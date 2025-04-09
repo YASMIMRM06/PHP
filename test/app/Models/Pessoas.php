12ba3f68afae9800e965725275aad213f6d2a8df
Ana Paula Lourenco <apnl.lourenco@gmail.com>
2025-04-09 10:21:52 -0300

    commit

---
+++ test/app/Models/Pessoas.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\http\Request;
+use App\Models\Pessoas;
+
+class PessoasController extends Controller
+{
+    $pessoas=Pessoas;; all();
+    dd($pessoas);
+
+}
